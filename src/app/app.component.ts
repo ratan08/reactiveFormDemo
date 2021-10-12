591dc393540f025fe013089cd681e53d2ce3cee4
Ratan <008ratan@gmail.com>
2021-10-12 15:49:47 +0530

    added forn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card'; 
+import { MatChipInputEvent } from '@angular/material/chips';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,28 @@ import {MatCardModule} from '@angular/material/card';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rectiveFormDemo';
+  
+
+  loginForm!: FormGroup ;
+  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
+      name: [null, Validators.required],
+    });
+  }
+
+  submit() {
+    if (!this.loginForm.valid) {
+      alert("fill the form")
+    }else{
+      alert("Form submitted")
+    }
+    console.log(this.loginForm);
+  }
 }
